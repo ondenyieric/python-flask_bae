5dd88328a5550f9a2860969356de235c2001afa8
ondenyieric <eric.ondenyi@meltwater>
2017-05-07 23:36:46 +0000

    flask bae

---
+++ data.py
@@ -0,0 +1,24 @@
+def Articles():
+    article=[
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'lorem ipsum',
+            'author':'Brad Tra',
+            'create_date':'02.04.2012'
+            },
+            {
+            'id': 2,
+            'title':'Article two',
+            'body':'lorem ipsum',
+            'author':'Brad Tra',
+            'create_date':'02.04.2012'
+            },
+            {
+            'id': 3,
+            'title':'Article three',
+            'body':'lorem ipsum',
+            'author':'Brad Tra',
+            'create_date':'02.04.2012'}
+    ]
+    return article
--- flask API.md
+++ flask API.md
@@ -10,11 +10,17 @@ $ virtualenv  venv
 #activate your virtual environment
 $ source venv/bin/activate
 
-#define variables to use during development
+#define variables to intergrate during development
 $ export FLASK_APP="run.py"
-$ export SECRET="mamamilka"
+
 $ export APP_SETTINGS="development"
 $ export DATABASE_URL="postgresql://localhost/flask_api"
+
+
+#update and refresh your .bashrc
+
+$ echo "source `which activate.sh`" >> ~/.bashrc
+$ source ~/.bashrc
 
 
 
